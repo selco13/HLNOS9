0783c240e9605170941d057d0e961715ae1f7ba5
robbiebyrd <me@robbiebyrd.com>
2024-04-26 14:40:38 -0500

    Make make_audio_sprites runnable from root

--- bin/make_audio_sprites.sh
+++ bin/make_audio_sprites.sh
@@ -1,5 +1,5 @@
-dir=${1:-../resources/sounds}
-outputDir=${2:-../public/sounds}
+dir=${1:-./resources/sounds}
+outputDir=${2:-./public/sounds}
 outputPath=${2:-/sounds}
 fileExt=${3:-mp3}
 formats=${4:-ogg,m4a,mp3,ac3}
