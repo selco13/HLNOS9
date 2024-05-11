23418be586baf9b62e2f1a0477d1bea7c8998cd2
Robbie Byrd <me@robbiebyrd.com>
2024-05-11 00:09:39 -0500

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
-'use client';
+'use client'
 
-import Browser from "@/app/Applications/Browser/Browser";
-import Demo from "@/app/Applications/Demo/Demo";
-import SimpleText from "@/app/Applications/SimpleText/SimpleText";
-
-import {AppearanceManager} from "@/app/SystemFolder/ControlPanels/AppearanceManager/AppearanceManager";
-import {SoundManager} from "@/app/SystemFolder/ControlPanels/SoundManager/SoundManager";
-import {ClassicyDesktopProvider} from "@/app/SystemFolder/SystemResources/AppManager/ClassicyAppManagerContext";
-import ClassicyDesktop from "@/app/SystemFolder/SystemResources/Desktop/ClassicyDesktop";
-import React from "react";
+import Browser from '@/app/Applications/Browser/Browser'
+import Demo from '@/app/Applications/Demo/Demo'
+import SimpleText from '@/app/Applications/SimpleText/SimpleText'
+import ControlPanels from '@/app/SystemFolder/ControlPanels/ControlPanels'
+import { ClassicyDesktopProvider } from '@/app/SystemFolder/SystemResources/AppManager/ClassicyAppManagerContext'
+import ClassicyDesktop from '@/app/SystemFolder/SystemResources/Desktop/ClassicyDesktop'
+import React from 'react'
 
 export default function Home() {
     return (
         <ClassicyDesktopProvider>
             <ClassicyDesktop>
-                <AppearanceManager/>
-                <SoundManager/>
-                <Demo/>
-                <Browser/>
-                <SimpleText/>
+                <ControlPanels />
+                <Demo />
+                <Browser />
+                <SimpleText />
             </ClassicyDesktop>
         </ClassicyDesktopProvider>
-    );
-};
+    )
+}
